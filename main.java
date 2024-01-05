5dcf2188d10bf9f8254b55d23ebd68fc6e7e5fb4
Raneem <raneemabdulbar@gmail.com>
2024-01-06 02:21:18 +0300

    main stuff

--- main.java
+++ main.java
@@ -65,8 +65,108 @@ public class main{
         return -1; // Indicates failure to add a doctor
     }
 
+    public int addNurse(String firstName, String lastName, String username, String password, String email, String address, int salary, int phone) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet generatedKeys = null;
 
-    
+        try {
+            connection = DBConnection.getConnection();
+            String insertQuery = "INSERT INTO nurse(first_name, last_name, username, password, email, address, salary, phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+            preparedStatement = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setString(3, username);
+            preparedStatement.setString(4, password);
+            preparedStatement.setString(5, email);
+            preparedStatement.setString(6, address);
+            preparedStatement.setInt(7, salary);
+            preparedStatement.setInt(8, phone);
+
+            int rowsInserted = preparedStatement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("A new nurse was inserted.");
+
+                generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    int nurseId = generatedKeys.getInt(1);
+                    System.out.println("Generated nurse ID: " + nurseId);
+                    return nurseId;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (generatedKeys != null) {
+                    generatedKeys.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return -1; // Indicates failure to add a nurse
+    }
+
+    public int addReceptionist(String firstName, String lastName, String username, String password, String email, String address, int salary, int phone) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet generatedKeys = null;
+
+        try {
+            connection = DBConnection.getConnection();
+            String insertQuery = "INSERT INTO receptionist(first_name, last_name, username, password, email, address, salary, phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+            preparedStatement = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setString(3, username);
+            preparedStatement.setString(4, password);
+            preparedStatement.setString(5, email);
+            preparedStatement.setString(6, address);
+            preparedStatement.setInt(7, salary);
+            preparedStatement.setInt(8, phone);
+
+            int rowsInserted = preparedStatement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("A new receptionist was inserted.");
+
+                generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    int receptionistId = generatedKeys.getInt(1);
+                    System.out.println("Generated receptionist ID: " + receptionistId);
+                    return receptionistId;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (generatedKeys != null) {
+                    generatedKeys.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return -1; // Indicates failure to add a receptionist
+    }
+
 }
   
 
