2e2da6eb81ad9a232fe66314e96f6d3721080ef3
khaledsharaff <khaled.sh.02@gmail.com>
2024-01-14 20:59:45 +0300

    owarida

--- Entities/User.java
+++ Entities/User.java
@@ -1,5 +1,40 @@
 package Entities;
 
 public class User {
-    
+    private String username;
+    private String password;
+    private String role;
+
+    // Constructor
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getter methods
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    // Setter methods (optional)
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
